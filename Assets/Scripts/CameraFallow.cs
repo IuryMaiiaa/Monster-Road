ecd862218dbf83d487f06492442e0ff259a26ece
Neils01 <iurryy@yahoo.com.br>
2016-04-08 19:06:37 -0300

    commit inicial

---
+++ Assets/Scripts/CameraFallow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFallow : MonoBehaviour {
+    public Transform playerPosicao;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(playerPosicao.position.x+5, playerPosicao.position.y+15,
+                                                playerPosicao.position.z-10);
+	}
+}
---
+++ Assets/Scripts/PlayerMonstro.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMonstro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(new Vector3(0,0,0.5f));
+        if(Input.GetKeyDown("left"))
+        {
+            virarEsquerda();
+        } else if (Input.GetKeyDown("right"))
+        {
+            virarDireita();
+        }
+	}
+
+    public void virarDireita()
+    {
+        this.transform.Rotate(new Vector3(0,10,0));    
+    }
+
+    public void virarEsquerda()
+    {
+        this.transform.Rotate(new Vector3(0,-10, 0));
+    }
+}
