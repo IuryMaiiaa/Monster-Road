0a1caeb290e8a7635d6d713001ff9424b64ef6fd
Neils01 <iurryy@yahoo.com.br>
2016-04-23 19:09:51 -0300

    adicionando spaw de inimigos e seu gerenciador

---
+++ Assets/Scripts/GerenciadorDeInimigos.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GerenciadorDeInimigos : MonoBehaviour {
+    public SpawEnemy[] spawEnemies;
+    public GameObject inimigoNivel1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int realizarContagemInimigosVivos()
+    {
+        int inimigos = 0;
+        foreach(SpawEnemy enemy in spawEnemies)
+        {
+            if(enemy.inimigoVivo())
+            {
+                inimigos++;
+            }
+        }
+        return inimigos;
+    }
+}
---
+++ Assets/Scripts/SpawEnemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawEnemy : MonoBehaviour {
+    public GameObject inimigo;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void spawInimigo(GameObject inimigo)
+    {
+        inimigo = GameObject.Instantiate(inimigo) as GameObject;
+        inimigo.transform.position = new Vector3(this.transform.position.x,
+                                                 this.transform.position.y + 2,
+                                                 this.transform.position.z);
+    }
+
+    public bool inimigoVivo()
+    {
+        if(inimigo != null)
+        {
+            return false;
+        }
+        return true;
+    }
+}
